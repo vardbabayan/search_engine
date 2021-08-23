15a75a255d741f2f4efd8b03db0373c5e7200fee
VartanBabayan <babayan.vartan@student.rau.am>
2021-08-23 16:47:28 +0400

    updated script to install mysql

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,8 @@
 LIBCURL=libcurlpp-dev
 LIBGUMBO=libgumbo-dev
 LIBMYSQL=libmysqlcppconn-dev
+MYSQL_SERVER=mysql-server
+MYSQL_CLIENT=mysql-client
 
-sudo apt-get install $LIBCURL $LIBGUMBO $LIBMYSQL -y
+sudo apt update
+sudo apt-get install $LIBCURL $LIBGUMBO $LIBMYSQL $MYSQL_SERVER $MYSQL_CLIENT -y
